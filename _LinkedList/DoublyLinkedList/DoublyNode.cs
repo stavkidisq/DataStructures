a29e79b539012281d372c06ce4f1c7dd5fc1a4e6
AllStoneFirst <57217014+AllStoneFirst@users.noreply.github.com>
2020-10-07 20:26:20 +0300

    change DoublyNode.cs and DoublyLinkedList.cs

--- _LinkedList/DoublyLinkedList/DoublyNode.cs
+++ _LinkedList/DoublyLinkedList/DoublyNode.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace DoublyLinkedList
 {
-    public class DoublyNode
+    public class DoublyNode<T>
     {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+
+        public T Data { get; set; }
     }
 }
