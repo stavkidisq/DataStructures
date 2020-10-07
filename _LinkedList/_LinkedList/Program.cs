3645ab6575d336172b71e1ea5f335cf0ce2cba76
AllStoneFirst <57217014+AllStoneFirst@users.noreply.github.com>
2020-10-07 16:56:51 +0300

    redacted file Program

--- _LinkedList/_LinkedList/Program.cs
+++ _LinkedList/_LinkedList/Program.cs
@@ -6,7 +6,19 @@ namespace _LinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinkedList<int> linkedList = new LinkedList<int>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                linkedList.Add(new Random().Next(0, 100));
+            }
+
+            Node<int> node;
+
+            for(node = linkedList.First; node != null; node = node.Next)
+            {
+                Console.Write(node.Data + " ");
+            }
         }
     }
 }
