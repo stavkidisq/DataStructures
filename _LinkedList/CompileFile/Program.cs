8a3af4a2acc39cdc34aec1e869474ed3563d0ba9
AllStoneFirst <57217014+AllStoneFirst@users.noreply.github.com>
2020-10-07 22:16:27 +0300

    change project archeticture

--- _LinkedList/CompileFile/Program.cs
+++ _LinkedList/CompileFile/Program.cs
@@ -1,5 +1,7 @@
-﻿
+﻿using _LinkedList;
+using DoublyLinkedList;
 using System;
+using System.Collections;
 
 namespace CompileFile
 {
@@ -7,7 +9,19 @@ namespace CompileFile
     {
         static void Main(string[] args)
         {
+            LinkedList<int> linkedList = new LinkedList<int>() { 1, 2, 3, 4, 5 };
+            DoublyLinkedList<int> DlinkedList = new DoublyLinkedList<int>() { 6, 4, 8, 9, 1, 4 };
 
+            DisplayInList(linkedList);
+            DisplayInList(DlinkedList);
+        }
+
+        private static void DisplayInList<T>(System.Collections.Generic.IEnumerable<T> list)
+        {
+            foreach(var item in list)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
