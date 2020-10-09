de84f2dc098d669efa7d3379d55632472750ae16
AllStoneFirst <57217014+AllStoneFirst@users.noreply.github.com>
2020-10-09 19:53:44 +0300

    added file Queue.cs

---
+++ _LinkedList/DataStructures/Queue.cs
@@ -0,0 +1,106 @@
+﻿using _LinkedList;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class Queue<T> : IEnumerable<T>
+    {
+        Node<T> Head;
+        Node<T> Tail;
+        int count;
+
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            Node<T> current = Tail;
+
+            Tail = node;
+
+            if (count == 0)
+                Head = Tail;
+            else
+                current.Next = Tail;
+
+            count++;
+        }
+
+        public T Dequeue(T data)
+        {
+            if (count == 0)
+                new InvalidCastException("Очередь пуста!");
+
+            T value = Head.Data;
+            Head = Head.Next;
+            count--;
+
+            return value;
+        }
+
+        public T First
+        {
+            get
+            {
+                if (count == 0)
+                    new InvalidCastException("Очередь пуста!");
+
+                return Head.Data;
+            }
+        }
+
+        public T Last
+        {
+            get
+            {
+                if (count == 0)
+                    new InvalidCastException("Очередь пуста!");
+
+                return Tail.Data;
+            }
+        }
+
+        public int Count => count;
+        public bool IsEmpty => count == 0;
+
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            count = 0;
+        }
+
+        public bool Contains(T data)
+        {
+            var current = Head;
+
+            while(current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = Head;
+
+            while(current != null)
+            {
+                yield return current.Data;
+
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
