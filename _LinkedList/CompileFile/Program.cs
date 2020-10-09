17b84f3a5ebd98a6cc29a0b93ca634219281ffd7
AllStoneFirst <57217014+AllStoneFirst@users.noreply.github.com>
2020-10-09 09:35:32 +0300

    added stack.cs and fixed program.cs

--- _LinkedList/CompileFile/Program.cs
+++ _LinkedList/CompileFile/Program.cs
@@ -1,7 +1,7 @@
 ﻿using _LinkedList;
+using DataStructures;
 using DoublyLinkedList;
 using System;
-using System.Collections;
 
 namespace CompileFile
 {
@@ -11,9 +11,12 @@ namespace CompileFile
         {
             LinkedList<int> linkedList = new LinkedList<int>() { 1, 2, 3, 4, 5 };
             DoublyLinkedList<int> DlinkedList = new DoublyLinkedList<int>() { 6, 4, 8, 9, 1, 4 };
+            Stack<int> stack = new Stack<int>();
 
             DisplayInList(linkedList);
             DisplayInList(DlinkedList);
+            PushStack(stack);
+            DisplayInList(stack);
         }
 
         private static void DisplayInList<T>(System.Collections.Generic.IEnumerable<T> list)
@@ -21,6 +24,15 @@ namespace CompileFile
             foreach(var item in list)
             {
                 Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void PushStack(Stack<int> stack)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                stack.Push(new Random().Next(0, 10));
             }
         }
     }
---
+++ _LinkedList/DataStructures/Stack.cs
@@ -0,0 +1,64 @@
+﻿using _LinkedList;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        Node<T> Head;
+        int count;
+
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Push(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            node.Next = Head;
+            Head = node;
+            count++;
+        }
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                new InvalidCastException("Стек пуст!");
+            }
+
+            Node<T> node = Head;
+            Head = node.Next;
+            count--;
+
+            return node.Data;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                new InvalidCastException("Стек пуст!");
+            }
+
+            return Head.Data;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = Head;
+            while(current != null)
+            {
+                yield return current.Data;
+
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
