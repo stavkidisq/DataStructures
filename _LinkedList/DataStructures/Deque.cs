17ae79309e5671e5cc8b1f72e0c4aad9cfbebd8a
AllStoneFirst <57217014+AllStoneFirst@users.noreply.github.com>
2020-10-11 21:34:16 +0300

    create Deque.cs

---
+++ _LinkedList/DataStructures/Deque.cs
@@ -0,0 +1,50 @@
+﻿using DoublyLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace DataStructures
+{
+    public class Deque<T>
+    {
+        int count;
+        DoublyNode<T> Head;
+        DoublyNode<T> Tail;
+
+        public void AddLast(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+
+            if(Head == null)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+                node.Previous = Tail;
+            }
+
+            Tail = node;
+            count++;
+        }
+
+        public void AddFirst(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+            DoublyNode<T> temp = Head;
+            node.Next = Head;
+
+            if (count == 0)
+            {
+                Tail = Head;
+            }
+            else
+            {
+                temp.Previous = node;
+            }
+            count++;
+        }
+    }
+}
